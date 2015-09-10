5fd8f88aab2310ceef99a6860b3d94d77686b06c
Dan Liew <daniel.liew@imperial.ac.uk>
2015-09-09 22:42:41 -0700

    hack in some timing code.

--- test/generator/demo_x86_aottest.cpp
+++ test/generator/demo_x86_aottest.cpp
@@ -7,6 +7,8 @@
 #include "demo_x86_vector.h"
 #include "demo_x86_no_half.h"
 #include "halide_image.h"
+#include <chrono>
+#include <iostream>
 
 // GROSS
 #include "emmintrin.h"
@@ -17,6 +19,9 @@ const int kSize = 10000;
 
 int main(int argc, char **argv) {
 
+  typedef std::chrono::duration<double> duration_t;
+  std::chrono::time_point<std::chrono::high_resolution_clock> vStart, vEnd, sStart, sEnd,
+    floatStart, floatEnd, initStart, initEnd;
   float param = 1.5;
 
   Image<uint16_t> input(kSize, kSize, 1); // float16 bits
@@ -26,6 +31,7 @@ int main(int argc, char **argv) {
 
   // FIXME: This is too slow!
   printf("Initialise %ux%u img\n", kSize, kSize);
+  initStart = std::chrono::high_resolution_clock::now();
   // Initialise data
   for (int x=0; x < kSize; ++x) {
     for (int y=0; y < kSize; ++y) {
@@ -35,8 +41,8 @@ int main(int argc, char **argv) {
         inputAsFloat(x, y) = halide_float16_bits_to_float(halfBits);
     }
   }
+  initEnd = std::chrono::high_resolution_clock::now();
   printf("Finished initialising\n");
-
 
   // EVIL!
   #define FLUSH do { _mm_clflush(input.data()); _mm_clflush(inputAsFloat.data());} while(0);
@@ -44,22 +50,36 @@ int main(int argc, char **argv) {
   FLUSH
 
   printf("Starting vector\n");
+  vStart = std::chrono::high_resolution_clock::now();
   demo_x86_vector(input, param, output);
+  vEnd = std::chrono::high_resolution_clock::now();
   printf("Finished vector\n");
 
   FLUSH
 
   printf("Starting pure float impl\n");
+  sStart = std::chrono::high_resolution_clock::now();
   demo_x86_no_half(inputAsFloat, param, outputAsFloat);
+  sEnd = std::chrono::high_resolution_clock::now();
   printf("Finishing pure float impl\n");
 
   FLUSH
 
   printf("Starting soft\n");
+  floatStart = std::chrono::high_resolution_clock::now();
   demo_x86_soft(input, param, output);
+  floatEnd = std::chrono::high_resolution_clock::now();
   printf("Finished soft\n");
 
+  duration_t vTime = vEnd - vStart;
+  duration_t sTime = sEnd - sStart;
+  duration_t floatTime = floatEnd - floatStart;
+  duration_t initTime = initEnd - initStart;
 
+  std::cout << "init time:" << initTime.count() << " secs\n";
+  std::cout << "vector time:" << vTime.count() << " secs\n";
+  std::cout << "soft time:" << sTime.count() << " secs\n";
+  std::cout << "float (no half) time:" << floatTime.count() << " secs\n";
 
   printf("Success!\n");
   return 0;
