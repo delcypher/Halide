8dfcd97edcce8e33ec176319c61669507a099760
Dan Liew <daniel.liew@imperial.ac.uk>
2015-09-09 22:27:35 -0700

    Add hack to initialise data. Perhaps we should use halide to do this for us which would be faster.

--- test/generator/demo_x86_aottest.cpp
+++ test/generator/demo_x86_aottest.cpp
@@ -13,17 +13,30 @@
 
 using namespace Halide::Tools;
 
-const int kSize = 4096 *10;
+const int kSize = 10000;
 
 int main(int argc, char **argv) {
 
-  // FIXME: Initialise data!
+  float param = 1.5;
+
   Image<uint16_t> input(kSize, kSize, 1); // float16 bits
   Image<uint16_t> output(kSize, kSize, 1);
   Image<float> inputAsFloat(kSize, kSize, 1);
   Image<float> outputAsFloat(kSize, kSize, 1);
 
-  float param = 1.5;
+  // FIXME: This is too slow!
+  printf("Initialise %ux%u img\n", kSize, kSize);
+  // Initialise data
+  for (int x=0; x < kSize; ++x) {
+    for (int y=0; y < kSize; ++y) {
+        // Only positive halfs, no NaN or inf
+        uint16_t halfBits = (x + y*kSize) % 0x7bff;
+        input(x, y) = halfBits;
+        inputAsFloat(x, y) = halide_float16_bits_to_float(halfBits);
+    }
+  }
+  printf("Finished initialising\n");
+
 
   // EVIL!
   #define FLUSH do { _mm_clflush(input.data()); _mm_clflush(inputAsFloat.data());} while(0);
